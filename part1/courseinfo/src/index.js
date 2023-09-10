b8e2cb705789bd4373f38c6313a2e9a040a3d0e7
Cesar Alvarez Marin <cesaralvarezmarin@gmail.com>
2023-09-10 13:10:58 +0200

    exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,42 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const course = 'Half Stack application development';
+
+const courseContent= {
+  part1 : {
+    partName : "Fundamentals of React",
+    partExercises : 5
+  },
+  part2 : {
+    partName : "Using props to pass data",
+    partExercises : 7
+  },    
+  part3 : {
+    partName : "State of a component",
+    partExercises : 14
+  }
+}
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
-const Content = (props) => {
+function Container(props) {
+  const { content } = props;
+
   return (
     <>
-      <p>{props.part1}: {props.exercises1}</p>
-      <p>{props.part2}: {props.exercises2}</p>
-      <p>{props.part3}: {props.exercises3}</p>
+      {Object.keys(content).map((partKey) => (
+        <Part
+          key={partKey}
+          partName={content[partKey].partName}
+          partExercises={content[partKey].partExercises}
+        />
+      ))}
     </>
-  )
+  );
 }
 
-const Total = (props) => {
+function Part(props) {
+  const { partName, partExercises } = props;
+
   return (
-    <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
+    <>
+      <h3>{partName}</h3>
+      <p>Number of Exercises: {partExercises}</p>
+    </>
+  );
+}
+
+function Total(props) {
+  const { content } = props;
+
+  const totalExercises = Object.values(content).reduce(
+    (acc, part) => acc + part.partExercises,
+    0
+  );
+
+  return (
+    <>
+      <h3>Total Exercises:</h3>
+      <p>{totalExercises}</p>
+    </>
+  );
 }
 
 function App() {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  console.log(courseContent.part1)
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Container content={courseContent} />
+      <Total content={courseContent} />
     </div>
   );
 }
