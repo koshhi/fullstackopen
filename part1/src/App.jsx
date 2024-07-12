2f54bbf599e48c2d7a6b18bcbafac4255d5ed002
Cesar Alvarez Marin <cesaralvarezmarin@gmail.com>
2024-07-12 14:11:47 +0200

    Refactor components and render Part components correctly

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Header = (props) => {
-  //console.log(props.course)
+  //console.log(props)
   return (
     <div>
       <h1>
@@ -11,60 +11,78 @@ export const Header = (props) => {
   )
 }
 
-export const Part = (props) => {
+
+const Part = (props) => {
+  //console.log(props)
   return (
     <p>
-      {props.name}: {props.exercises}
+      {props.part.name}: {props.part.exercises}
     </p>
   );
 }
 
-export const Content = (props) => {
-  console.log(props)
+const Content = (props) => {
+  //console.log(props)
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
     </div>
   );
 }
 
-export const Total = (props) => {
-  //console.log(props)
+
+const Total = (props) => {
+  console.log(props)
+  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
+    <p>Number of exercises {totalExercises}</p>
+  );
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course}></Header>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+}
+
+
+export default App
+
+
+/*
       <Content 
-        part1={part1} 
-        exercises1={exercises1} 
-        part2={part2} 
-        exercises2={exercises2} 
-        part3={part3}
-        exercises3={exercises3}
+        part1={part1.name} 
+        exercises1={part1.exercises} 
+        part2={part2.name} 
+        exercises2={part2.exercises} 
+        part3={part3.name}
+        exercises3={part3.exercises}
       />
       <Total 
         exercises1={exercises1} 
         exercises2={exercises2} 
         exercises3={exercises3}
       />
-    </div>
-  )
-}
-
-
-export default App
+*/
