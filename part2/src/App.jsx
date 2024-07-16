a0e5487b146b2ee8a119d74e2b55d1310dc27afc
Cesar Alvarez Marin <cesaralvarezmarin@gmail.com>
2024-07-16 17:19:18 +0200

    exercise 2.6-2.9

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,57 +1,74 @@
-import {Course} from "./Course"
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    // Evitar agregar nombres duplicados
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
-  ]
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-  <div>
-    {courses.map(course => (
-      <Course key={course.id} course={course} />
-    ))}
-  </div>
+    <div>
+      <h2>Phonebook</h2>
+        <div>
+          Search by name:  <input value={searchTerm} onChange={handleSearchChange} />
+        </div>
+      <form onSubmit={addPerson}>
+        <h4>Add numbers</h4>
+        <div>name: <input onChange={handleNameChange} value={newName} /></div>
+        <div>number: <input onChange={handleNumberChange} value={newNumber} /></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {personsToShow.map(person => (
+          <li key={person.id}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+      <div>debug: {newName}, {newNumber}, {searchTerm}</div>
+    </div>
   )
 }
 
